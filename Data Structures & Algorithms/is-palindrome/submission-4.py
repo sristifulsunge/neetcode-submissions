f54bb1e08114aa2a6527e0242a1d2f2ab6800228
Sristi Fulsunge <158131125+sristifulsunge@users.noreply.github.com>
2026-06-03 18:05:17 -0700

    Bulk sync: 8 submissions

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    #using two pointers 
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s)-1
+    
+        while i<j:
+            if not s[i].isalnum():
+                i= i+ 1
+                continue
+        
+            if not s[j].isalnum():
+                j = j-1
+                continue
+                
+            if s[i].lower() == s[j].lower():
+                i = i+1
+                j = j-1
+            else:
+                return False
+
+        return True
