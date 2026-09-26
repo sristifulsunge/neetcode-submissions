e070d80f3be5f742d5010c9182582e3b50e54e5a
Sristi Fulsunge <158131125+sristifulsunge@users.noreply.github.com>
2026-09-25 17:42:56 -0700

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #using two pointer method 
-        min_buy = float('inf')
-        max_sell = 0
-        max_profit = 0
+        maxProfit = 0
+        minBuy = prices[0]
 
-        for price in prices:
-            if price < min_buy:
-                min_buy = price
-            profit = price - min_buy
-            max_profit = max(max_profit, profit)
+        for sell in prices:
+            maxProfit = max(maxProfit, sell - minBuy)
+            minBuy = min(minBuy, sell)
 
-        return max_profit
+        return maxProfit
